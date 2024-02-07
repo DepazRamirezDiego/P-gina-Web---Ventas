fe033cac9078d2a3069d821e996147f92473653a
Diego Depaz Ramirez <depazramirezdiego02@gmail.com>
2024-02-06 21:54:30 -0500

    Mejorando archivo script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    sliderPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nexEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+})
