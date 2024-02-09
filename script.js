869ebde218f648fd458e8a1e42a1282c57bd815a
Diego Depaz Ramirez <depazramirezdiego02@gmail.com>
2024-02-08 23:11:47 -0500

    Mejorando archivos index.html, styles.css, script.js | Creando nuevo Footer

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ var swiper = new Swiper(".slide-content", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+/*Validación y error de formulario*/
+function submitForm() {
+    var emailInput = document.getElementById("emailInput").value;
+    var acceptTerms = document.getElementById("acceptTerms").checked;
+    
+    var gmail = /@gmail\.com$/;
+    var hotmail = /@hotmail\.com$/;
+    
+    if (emailInput && acceptTerms) {
+        if (gmail.test(emailInput) || hotmail.test(emailInput)) {
+            console.log("Correo electrónico válido: " + emailInput);
+            console.log("Términos y condiciones aceptados");
+        } else {
+            alert("Por favor, ingrese un correo electrónico Gmail o Hotmail válido.");
+        }
+    } else {
+        alert("Por favor, ingrese su correo electrónico y acepte los Términos y Condiciones.");
+    }
+}
