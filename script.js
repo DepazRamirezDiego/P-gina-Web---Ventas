827905e2bffef6e025f39d54efbab63a4d08d333
Diego Depaz Ramirez <depazramirezdiego02@gmail.com>
2024-02-10 00:13:26 -0500

    Mejorando el aspecto del Header en la rama PruebaHeader | Creando NavBar con efecto hover - Creando funcionalidad de botones de compra

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+/*Script nav indicator*/
+const list = document.querySelectorAll(".list");
+
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
+
+/*Script - Shopping-Cart*/
+const carrito = document.getElementById('shopping-cart');
+const elementos = document.querySelectorAll('.add-cart');
+const lista = document.querySelector('#list-cart tbody');
+const vaciarCarritoBtn = document.getElementById('empty-cart');
+
+cargarEventoListeners();
+
+function cargarEventoListeners(){
+    elementos.forEach(elemento => {
+        elemento.addEventListener('click', comprarElemento);
+    });
+    carrito.addEventListener('click', eliminarElemento)
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('add-cart')){
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento){
+    const tituloElemento = elemento.querySelector('h4, h1, h3');
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: tituloElemento ? tituloElemento.textContent : 'Título no encontrado',
+        precio: elemento.querySelector('.price').textContent,
+        id: elemento.querySelector('.add-cart').getAttribute('data-id')
+    };
+    insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=50/>
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a href="#" class="borrar" data-id="${elemento.id}">X</a>
+        </td>
+    `;
+
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e){
+    e.preventDefault();
+    let elemento,
+        elementoId;
+    
+    if(e.target.classList.contains('borrar')){
+        e.target.parentElement.parentElement.remove();
+        elemento=e.target.parentElement.parentElement;
+        elementoId=elemento.querySelector('.borrar').getAttribute('data-id');
+    }
+}
+
+function vaciarCarrito(){
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+
+    return false;
+}
+
+
+/*Script Slider Card*/
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 30,
