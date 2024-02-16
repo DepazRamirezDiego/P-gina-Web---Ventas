c7b781bdfd2fe8f881fc6f796529e7a40de08623
Diego Depaz Ramirez <depazramirezdiego02@gmail.com>
2024-02-15 21:38:05 -0500

    Mejorando archivo index.html, styles.css y script.js | Mejorando Header en rama PruebaHeader2

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-/*Script nav indicator*/
-const list = document.querySelectorAll(".list");
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
 
-function activeLink() {
-  list.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
+function mostrar_menu(){
+
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
 }
-list.forEach((item) => item.addEventListener("click", activeLink));
+
+
+                            //Buscador de contenido
+
+//Ejecutando funciones
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+//Declarando variables
+bars_search =       document.getElementById("ctn-bars-search");
+cover_ctn_search =  document.getElementById("cover-ctn-search");
+inputSearch =       document.getElementById("inputSearch");
+box_search =        document.getElementById("box-search");
+
+
+//Funcion para mostrar el buscador
+function mostrar_buscador(){
+
+    bars_search.style.top = "80px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+
+    if (inputSearch.value === ""){
+        box_search.style.display = "none";
+    }
+
+}
 
 /*Script - Shopping-Cart*/
 const carrito = document.getElementById('shopping-cart');
@@ -83,30 +109,6 @@ function vaciarCarrito(){
     return false;
 }
 
-/*Search - Bar*/
-//Ejecutando funciones
-document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
-document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
-
-//Declarando variables
-bars_search =       document.getElementById("ctn-bars-search");
-cover_ctn_search =  document.getElementById("cover-ctn-search");
-inputSearch =       document.getElementById("inputSearch");
-box_search =        document.getElementById("box-search");
-
-
-//Funcion para mostrar el buscador
-function mostrar_buscador(){
-
-    bars_search.style.top = "80px";
-    cover_ctn_search.style.display = "block";
-    inputSearch.focus();
-
-    if (inputSearch.value === ""){
-        box_search.style.display = "none";
-    }
-
-}
 
 //Funcion para ocultar el buscador
 function ocultar_buscador(){
@@ -155,7 +157,6 @@ function buscador_interno(){
 }
 
 
-
 /*Script Slider Card*/
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
@@ -193,3 +194,4 @@ function submitForm() {
         alert("Por favor, ingrese su correo electrónico y acepte los Términos y Condiciones.");
     }
 }
+
